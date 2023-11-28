82795e981bc752e84cc52673f82eaa461cf5e6af
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-28 19:15:25 +0500

    show name based on isnameshowing bool var

--- Learning_React/2. JS_mastery/hello/src/App.js
+++ Learning_React/2. JS_mastery/hello/src/App.js
@@ -4,14 +4,22 @@ import './App.css';
 const App = () => { 
 
   const name = "Rahim";
-  const isNameShowing = 0; 
+  const isNameShowing = 1; 
   // different display based on different dynamic nice
 
   return (
     // classname is a different
     //class is reserved in java
     <div className="App">
-      <h1>Hello, {isNameShowing ? name : "someone"}! </h1>
+      <h1>Hello, {isNameShowing ? (
+        <>
+          <h3>{name}!</h3>
+        </>
+      ) : (
+        <>
+          <h2>There is no name!</h2>
+        </>
+      )} </h1>
     </div>
   );
 }
@@ -19,3 +27,5 @@ const App = () => {
 export default App;
 
 // dynamcally rendering real data inside browser
+// react frarment
+// things need to be wrapped in react fragmenets
