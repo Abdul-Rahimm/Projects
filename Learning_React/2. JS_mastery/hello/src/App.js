1b685131811bcfd08b79cd8c8eea317370ce94dc
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-28 19:04:47 +0500

    making changes to the first react file

--- Learning_React/2. JS_mastery/hello/src/App.js
+++ Learning_React/2. JS_mastery/hello/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// functional component
+const App = () => { 
   return (
+    // classname is a different
+    //class is reserved in java
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello React world</h1>
     </div>
   );
 }
 
 export default App;
+
+// dynamcally rendering real data inside browser
