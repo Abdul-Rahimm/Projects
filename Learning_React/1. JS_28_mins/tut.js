da245f9a6014b7a7a1e59af96e05e1f55b2faf65
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-28 14:55:10 +0500

    1. learned arrow functions

---
+++ Learning_React/1. JS_28_mins/tut.js
@@ -0,0 +1,72 @@
+// Arrow functions
+
+const func = () => {
+    console.log("hello world");
+}
+const component = (a, b) => {
+    return a+b;
+}
+export const c1 = (a, b) => {
+    return a+b;
+}
+
+<button onclick = {
+    () => {
+        console.log("hello");
+    }
+}>
+</button>
+
+// --------------------------------------------------------------------------------------------------------------
+
+//ternary operators
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
