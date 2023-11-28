855155f5077ae9913830b589f3e52dce51408e33
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-28 17:40:48 +0500

    JS setup

---
+++ Learning_React/2. JS_mastery/JS_mastery.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const func = () => {
+    return <div>
+        hello world
+    </div>
+}
---
+++ Learning_React/React_Documentation/1.js
@@ -0,0 +1,14 @@
+function Mybutton(){
+    return <button>
+        Button
+    </button>
+}
+
+export default function MyApp(){
+    return <div>
+        <h1>
+            Welcome to My learning Journey
+            <Mybutton/>
+        </h1>
+    </div>
+}
