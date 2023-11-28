0f7bd1fa764886d1ebf71d43193e5e0d51011f39
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-28 15:08:05 +0500

    2 learned ternary operators

--- Learning_React/1. JS_28_mins/tut.js
+++ Learning_React/1. JS_28_mins/tut.js
@@ -6,24 +6,31 @@ const func = () => {
 const component = (a, b) => {
     return a+b;
 }
-export const c1 = (a, b) => {
-    return a+b;
-}
+// export const c1 = (a, b) => {
+//     return a+b;
+// }
 
-<button onclick = {
+{/* <button onclick = {
     () => {
         console.log("hello");
     }
 }>
-</button>
+</button> */}
 
 // --------------------------------------------------------------------------------------------------------------
+let age = 11;
+let div1 = 10, div2 = 100;
 
-//ternary operators
+let n = age == 10 && 10;
+let x = age == 11 || "abdul raheem";
+let name1 = age == 10 ? "10" : "not 10";
 
+const comp1 = () => {
+    return age = 11 ? div1 : div2;
+}
+console.log(comp1());
 
-
-
+// --------------------------------------------------------------------------------------------------------------
 
 
 
