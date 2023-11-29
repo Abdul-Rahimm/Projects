e0e17858bbc1e8df3acdbcb8c733615c1216d24c
Abdul-Rahimm <k213951@nu.edu.pk>
2023-11-29 08:32:15 +0500

    plus and minus counter with buttons. hooks

--- Learning_React/2. JS_mastery/hello/src/App.js
+++ Learning_React/2. JS_mastery/hello/src/App.js
@@ -1,31 +1,31 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-// functional component
-const App = () => { 
+// using hooks for states
+// events
 
-  const name = "Rahim";
-  const isNameShowing = 1; 
-  // different display based on different dynamic nice
+const App = () => {
+    const [counter, setCounter] = useState(0);
+    const [ count, setCount ] = useState(0);
 
+    useEffect(() => {
+        // alert("This happens as soon as this component is rendered")
+        setCounter(10);
+    }, [count])
+    // dependency array will make sure that the code only happens at the initiation
+    //whenever count is changed. it will set counter to 0
+    
   return (
-    // classname is a different
-    //class is reserved in java
     <div className="App">
-      <h1>Hello, {isNameShowing ? (
-        <>
-          <h3>{name}!</h3>
-        </>
-      ) : (
-        <>
-          <h2>There is no name!</h2>
-        </>
-      )} </h1>
+        <button onClick = {() => setCounter((prev) => prev - 1 )}>-</button>
+        <h1>{counter}</h1>
+        <button onClick = {() => setCounter((prev) => prev + 1 )}>+</button>
+        <br></br>
+        <button onClick = {() => setCount((prev) => prev - 1 )}>-</button>
+        <h1>{count}</h1>
+        <button onClick = {() => setCount((prev) => prev + 1 )}>+</button>
     </div>
   );
-}
+};
 
 export default App;
-
-// dynamcally rendering real data inside browser
-// react frarment
-// things need to be wrapped in react fragmenets
