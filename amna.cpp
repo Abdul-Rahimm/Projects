aeb7ac2bcdf969b118bd300cd97919ed46ab8a2d
Abdul-Rahimm <k213951@nu.edu.pk>
2023-12-02 17:34:23 +0500

    commiting this file to learn colaborative coding

---
+++ amna.cpp
@@ -0,0 +1,10 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+int main()
+{
+}
