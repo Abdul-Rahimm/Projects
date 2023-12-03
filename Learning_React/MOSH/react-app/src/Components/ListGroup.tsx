81f02cbc3083ad45391b152fc64f6f6d53968601
Abdul-Rahimm <k213951@nu.edu.pk>
2023-12-03 12:44:02 +0500

    learned about event handler. onclick event

--- Learning_React/MOSH/react-app/src/Components/ListGroup.tsx
+++ Learning_React/MOSH/react-app/src/Components/ListGroup.tsx
@@ -1,12 +1,18 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = ["new york", "pakistan", "karachi"];
+  // event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>Unordered List</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li key={item} className="list-group-item" onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
