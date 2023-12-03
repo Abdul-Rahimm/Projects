572d3391b24f3d8cf9c182e3870ab9f91c284352
Abdul-Rahimm <k213951@nu.edu.pk>
2023-12-03 12:52:51 +0500

    learned about useState hook. state changes

--- Learning_React/MOSH/react-app/src/Components/ListGroup.tsx
+++ Learning_React/MOSH/react-app/src/Components/ListGroup.tsx
@@ -1,16 +1,32 @@
-import { MouseEvent } from "react";
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
 
 function ListGroup() {
   const items = ["new york", "pakistan", "karachi"];
+
   // event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // const handleClick = (event: MouseEvent) => console.log(event);
+
+  // data or state that might change over time
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>Unordered List</h1>
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item" onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
